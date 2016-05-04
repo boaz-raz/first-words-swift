c8498a42b4117117787e6c7c7cd9016f23cad041
Boaz Raz <raz.boaz@gmail.com>
2016-05-04 18:48:35 -0400

    fix layout

--- First Words Final Project/PhotoGallaryViewController.swift
+++ First Words Final Project/PhotoGallaryViewController.swift
@@ -1,55 +1,48 @@
 //
 //  PhotoGallaryViewController.swift
-//  First Words Final Project
+//  My First Words
 //
-//  Created by Boaz Raz on 5/3/16.
+//  Created by Boaz Raz on 5/4/16.
 //  Copyright © 2016 Boaz Raz. All rights reserved.
 //
 
 import UIKit
-//import Photos
 
-// Concttoin
 let reuseID = "PhotoCell"
 
-class PhotoGallaryViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
+class PhotoGallaryViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
-    // Action
     
     @IBAction func btnCamera(sender: AnyObject) {
+        print("Camera")
     }
     
-    @IBAction func btnPhotoAlbum(sender: AnyObject) {
+    
+    @IBAction func btnAlbum(sender: AnyObject) {
+        print("Album")
     }
     
-    // Refrence to the collection view
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    //UICollection 
-
-    
+    @available(iOS 6.0, *)
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         
-        // Loop
         return 1;
-        
     }
     
     // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
-    
+    @available(iOS 6.0, *)
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell: UICollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseID, forIndexPath: indexPath) as UICollectionViewCell
-        
-        // Modify the cell
-        //cell.backgroundColor = UIColor.redColor()
         
         return cell
         
     }
-
     
-} // END CLASS
+    
+    
+}
--- ViewPhotoController.swift
+++ ViewPhotoController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewPhotoController.swift
-//  First Words Final Project
+//  My First Words
 //
 //  Created by Boaz Raz on 5/4/16.
 //  Copyright © 2016 Boaz Raz. All rights reserved.
@@ -10,27 +10,27 @@ import UIKit
 
 class ViewPhotoController: UIViewController {
     
+    
+    
     @IBAction func btnCancel(sender: AnyObject) {
         print("Cancel")
-        //self.navigationController?.popViewControllerAnimated(true)
     }
-    
     
     @IBAction func btnShare(sender: AnyObject) {
         print("Share")
     }
     
     
-    @IBAction func btnDelete(sender: AnyObject) {
-        print("Delete")
-    }
+//    @IBAction func btnDelete(sender: AnyObject) {
+//        print("Delete")
+//    }
     
+
     
-    @IBOutlet weak var imgFullSize: UIImageView!
+    //@IBOutlet weak var imageView: UIImageView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
-    
-} // END CLASS
+}
